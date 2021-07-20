8393f8f3b0277e9bddd87e51877e5691e961d77c
EC2 Default User <ec2-user@ip-10-1-1-162.ap-northeast-1.compute.internal>
2021-07-20 08:20:06 +0000

    firtst-commit

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models'
 require 'dotenv/load'
+enable :session
 
 get '/' do
     erb :index
+end
+
+post '/login' do
+    
 end
